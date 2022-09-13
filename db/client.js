08c29d30fdf36f0c34ad2953fbabfd529e1ab4bf
yeisonnn <yeisonnn@gmail.com>
2022-09-13 07:08:45 -0500

    cors added

--- db/client.js
+++ db/client.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const client = require('./db/client');
 const { PORT = 3000 } = process.env;
 const morgan = require('morgan');
 const appRouter = require('./api/index');
+const cors = require('cors');
 
+server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
 server.use('/', appRouter);
